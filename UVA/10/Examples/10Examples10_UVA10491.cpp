5b2c835cfc510e09cd4fdf33156b3ae9ac0e0c99
Langjiqianli <m15275430201@163.com>
2023-07-02 18:38:08 +0800

    97

---
+++ UVA/10/Examples/10Examples10_UVA10491.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double a,b,c;
+    while(cin>>a>>b>>c){
+        printf("%.5lf\n",(a*b+b*(b-1))/((a+b)*(a+b-c-1)));
+    }
+    system("pause");
+    return 0;
+}
